42d745e949016410cc555fd9c86768a47336f994
XDDDD <269294523@qq.com>
2020-10-26 19:56:48 +0800

    小修小补

--- src/main/java/cn/hdustea/aha_server/entity/Resume.java
+++ src/main/java/cn/hdustea/aha_server/entity/Resume.java
@@ -28,10 +28,10 @@ public class Resume {
     private String currentGrade;
     private String workPlace;
     private String profession;
-    private List<EduExperience> eduExpers;
-    private List<SchoolExperience> schoolExpers;
-    private List<ProjectExperience> projectExpers;
-    private List<PracticeExperience> practiceExpers;
+    private List<EduExperience> eduExperiences;
+    private List<SchoolExperience> schoolExperiences;
+    private List<ProjectExperience> projectExperiences;
+    private List<PracticeExperience> practiceExperiences;
     private String projectSkill;
     private List<Honor> honors;
     private String selfDescription;
