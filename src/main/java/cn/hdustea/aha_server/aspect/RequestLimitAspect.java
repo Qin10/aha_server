cead5cbfea11ffce47124a2ca69624c6fdd4c5db
XDDDD <269294523@qq.com>
2020-11-27 18:14:31 +0800

    完善了javadoc

--- src/main/java/cn/hdustea/aha_server/aspect/RequestLimitAspect.java
+++ src/main/java/cn/hdustea/aha_server/aspect/RequestLimitAspect.java
@@ -1,19 +1,12 @@
 package cn.hdustea.aha_server.aspect;
 
 import cn.hdustea.aha_server.annotation.RequestLimit;
-import cn.hdustea.aha_server.exception.runtimeApiException.RequestTimesExceededException;
-import cn.hdustea.aha_server.util.IpUtil;
 import cn.hdustea.aha_server.util.RedisUtil;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * 访问次数限制切面类
@@ -30,25 +23,25 @@ public class RequestLimitAspect {
     public void pointCut(RequestLimit requestLimit) {
     }
 
-    @Before(value = "pointCut(requestLimit)", argNames = "joinpoint,requestLimit")
-    public void before(JoinPoint joinpoint, RequestLimit requestLimit) throws RequestTimesExceededException {
-        ServletRequestAttributes attribute = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if (attribute != null) {
-            HttpServletRequest request = attribute.getRequest();
-            String ip = IpUtil.getIpAddr(request);
-            String className = joinpoint.getTarget().getClass().getName();
-            String methodName = joinpoint.getSignature().getName();
-            String key = RedisUtil.REQUEST_LIMIT_PREFIX + ip + ":" + className + "." + methodName;
-            Integer currentAmount = (Integer) redisUtil.get(key);
-            if (currentAmount == null) {
-                redisUtil.set(key, 1, requestLimit.time());
-            } else {
-                if (currentAmount > requestLimit.amount()) {
-                    throw new RequestTimesExceededException();
-                } else {
-                    redisUtil.incr(key, 1);
-                }
-            }
-        }
-    }
+//    @Before(value = "pointCut(requestLimit)", argNames = "joinpoint,requestLimit")
+//    public void before(JoinPoint joinpoint, RequestLimit requestLimit) throws RequestTimesExceededException {
+//        ServletRequestAttributes attribute = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//        if (attribute != null) {
+//            HttpServletRequest request = attribute.getRequest();
+//            String ip = IpUtil.getIpAddr(request);
+//            String className = joinpoint.getTarget().getClass().getName();
+//            String methodName = joinpoint.getSignature().getName();
+//            String key = RedisUtil.REQUEST_LIMIT_PREFIX + ip + ":" + className + "." + methodName;
+//            Integer currentAmount = (Integer) redisUtil.get(key);
+//            if (currentAmount == null) {
+//                redisUtil.set(key, 1, requestLimit.time());
+//            } else {
+//                if (currentAmount > requestLimit.amount()) {
+//                    throw new RequestTimesExceededException();
+//                } else {
+//                    redisUtil.incr(key, 1);
+//                }
+//            }
+//        }
+//    }
 }
